cf3530b374380f85fc72ab4c988e9d71933aaf2d
code-hoihoi <naoya2014@gmail.com>
2019-12-04 08:45:43 +0900

    Add some code via the lecture

--- flask_introduction/library/_02_html_inside_view.py
+++ flask_introduction/library/_02_html_inside_view.py
@@ -18,8 +18,20 @@ def hello_world():
     html = """
         <html>
             <h1>Welcome to our Library!</h1>
+            <ul>
+                <li>Edgar A. Poe</li>
+                <li>Jorge L. Borges</li>
+                <li>Mark Twain</li>
         </html>
     """
+    
     authors = ["Alan Poe", "Jorge L. Borges", "Mark Twain"]
     # build an <ul> with authors
-    return html
+    authors_list = "<ul>"
+    authors_list += "\n".join([
+        "<li>{author}</li>".format(author=author) for author in authors
+    ])
+    authors_list += "</ul>"
+    print(authors_list)
+    #return html
+    return authors_list
